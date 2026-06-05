4e94dbc9b2037606830744477416f7811f0db175
Johannes Poulsen <johannespoulsen@Mac.home>
2026-06-05 11:13:26 +0200

    simple genserver

--- lib/elixir/test/elixir/module/types/personel/simple_genserver.ex
+++ lib/elixir/test/elixir/module/types/personel/simple_genserver.ex
@@ -2,12 +2,24 @@ defmodule SimpleGenServer do
   use GenServer
 
   def start_link() do
-    {:ok, pid} = GenServer.start_link(__MODULE__, [])
+    {:ok, pid} = GenServer.start_link(__MODULE__, 0)
     pid
   end
 
-  def strict_add_one(pid, x) when is_integer(x) do
+  def add_one(pid, x) do
+    nested_add_one(pid, x)
+  end
+
+  def nested_add_one(pid, x) do
     GenServer.call(pid, {:add_one, x})
+  end
+
+  def strict_add_float(pid, x) when is_float(x) do
+    strict_nested_add_float(pid, x)
+  end
+
+  def strict_nested_add_float(pid, x) do
+    GenServer.cast(pid, {:add_float, x+1.2})
   end
 
   @impl true
@@ -25,11 +37,19 @@ defmodule SimpleGenServer do
     {:reply, x <> "Test", state}
   end
 
+  @impl true
+  def handle_cast({:add_float, x}, state) when is_float(x) do
+    y = x + 1.1
+    {:noreply, state + y}
+  end
+
   def main do
     pid = start_link()
-    # x = GenServer.call(pid, {:add_string, "Hello"})
-    # x <> " world"
-    x = strict_add_one(pid, 42)
-    x <> "Hello world"
+    x = GenServer.call(pid, {:add_one, 42})
+    # x = strict_add_float(pid, 42.0)
+    x <> " world"
+    # GenServer.cast(pid, {:add_float, "42.0"})
+    # add_one(pid, "42")
+    # x <> "Hello world"
   end
 end
