f0631cd7a5d0ca8cc0e211ed381d5acbe349ea76
Johannes Poulsen <johannespoulsen@Johanness-MacBook-Pro.local>
2026-06-22 15:04:40 +0200

    fix clean up

--- lib/elixir/lib/module/types/of.ex
+++ lib/elixir/lib/module/types/of.ex
@@ -98,59 +98,64 @@ defmodule Module.Types.Of do
       when is_integer(version) or is_reference(version) do
     %{vars: %{^version => %{type: old_type, off_traces: off_traces} = data} = vars} = context
 
-    context =
-      case context.conditional_vars do
-        %{} = conditional_vars ->
-          %{context | conditional_vars: Map.put(conditional_vars, version, true)}
+    # Dont refine the var if in a strict fun
+    if is_strict_fun(expr) and old_type != dynamic() do
+      {old_type, context}
+    else
+      context =
+        case context.conditional_vars do
+          %{} = conditional_vars ->
+            %{context | conditional_vars: Map.put(conditional_vars, version, true)}
 
-        nil ->
-          context
+          nil ->
+            context
+        end
+
+      case context do
+        _ when type in @skip_refinement_for or is_map_key(data, :errored) ->
+          {old_type, context}
+
+        %{pattern_info: %{guard_context: guard_context}} ->
+          new_type = intersection(old_type, type)
+
+          case empty?(new_type) do
+            true when guard_context == :orelse ->
+              data = %{
+                data
+                | type: none(),
+                  off_traces: new_trace(expr, none(), stack, off_traces)
+              }
+
+              {none(), %{context | vars: %{vars | version => data}}}
+
+            false when new_type != old_type ->
+              data = %{
+                data
+                | type: new_type,
+                  off_traces: new_trace(expr, new_type, stack, off_traces)
+              }
+
+              {new_type, %{context | vars: %{vars | version => data}}}
+
+            _ ->
+              {old_type, context}
+          end
+
+        _ ->
+          case gradual?(old_type) and compatible_intersection(old_type, type) do
+            {:ok, new_type} when new_type != old_type ->
+              data = %{
+                data
+                | type: new_type,
+                  off_traces: new_trace(expr, new_type, stack, off_traces)
+              }
+
+              {new_type, %{context | vars: %{vars | version => data}}}
+
+            _ ->
+              {old_type, context}
+          end
       end
-
-    case context do
-      _ when type in @skip_refinement_for or is_map_key(data, :errored) ->
-        {old_type, context}
-
-      %{pattern_info: %{guard_context: guard_context}} ->
-        new_type = intersection(old_type, type)
-
-        case empty?(new_type) do
-          true when guard_context == :orelse ->
-            data = %{
-              data
-              | type: none(),
-                off_traces: new_trace(expr, none(), stack, off_traces)
-            }
-
-            {none(), %{context | vars: %{vars | version => data}}}
-
-          false when new_type != old_type ->
-            data = %{
-              data
-              | type: new_type,
-                off_traces: new_trace(expr, new_type, stack, off_traces)
-            }
-
-            {new_type, %{context | vars: %{vars | version => data}}}
-
-          _ ->
-            {old_type, context}
-        end
-
-      _ ->
-        case gradual?(old_type) and compatible_intersection(old_type, type) do
-          {:ok, new_type} when new_type != old_type ->
-            data = %{
-              data
-              | type: new_type,
-                off_traces: new_trace(expr, new_type, stack, off_traces)
-            }
-
-            {new_type, %{context | vars: %{vars | version => data}}}
-
-          _ ->
-            {old_type, context}
-        end
     end
   end
 
